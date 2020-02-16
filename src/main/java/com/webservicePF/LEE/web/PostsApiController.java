0a5861642ff6c9289f12248581f4199f2a43b237
HYUNWOO <kiesland1121@naver.com>
2020-02-16 17:03:36 +0900

    BOOK CHAPTER 03

---
+++ src/main/java/com/webservicePF/LEE/web/PostsApiController.java
@@ -0,0 +1,42 @@
+package com.webservicePF.LEE.web;
+
+import com.webservicePF.LEE.service.posts.PostsService;
+import com.webservicePF.LEE.web.dto.PostsListResponseDto;
+import com.webservicePF.LEE.web.dto.PostsResponseDto;
+import com.webservicePF.LEE.web.dto.PostsSaveRequestDto;
+import com.webservicePF.LEE.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+/*
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id) {
+        postsService.delete(id);
+        return id;
+    }*/
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
+    }
+/*
+    @GetMapping("/api/v1/posts/list")
+    public List<PostsListResponseDto> findAll() {
+        return postsService.findAllDesc();
+    }*/
+}
